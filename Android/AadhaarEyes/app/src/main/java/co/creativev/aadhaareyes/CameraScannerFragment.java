52868af67a2db0a1ffdd420546ddffed6b6546d6
Amir <amir.nagri+git@gmail.com>
2015-06-06 22:35:19 +0530

    stabilization algo

--- Android/AadhaarEyes/app/src/main/java/co/creativev/aadhaareyes/CameraScannerFragment.java
+++ Android/AadhaarEyes/app/src/main/java/co/creativev/aadhaareyes/CameraScannerFragment.java
@@ -2,6 +2,7 @@ package co.creativev.aadhaareyes;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,13 +19,16 @@ import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class CameraScannerFragment extends Fragment implements CameraBridgeViewBase.CvCameraViewListener2 {
 
+    private final LinkedList<Mat> oldFrames;
     private JavaCameraView javaCameraView;
     private Mat screen;
     private MainActivity nativeBridge;
@@ -34,11 +38,17 @@ public class CameraScannerFragment extends Fragment implements CameraBridgeViewB
     private int num;
     private int[][] pt;
     private Scalar grayColor;
+    private Scalar black;
+    private Task task;
+    private Handler handler;
 
     public CameraScannerFragment() {
         nativeBridge = new MainActivity();
         random = new Random().nextInt();
         grayColor = Scalar.all(184);
+        black = Scalar.all(0);
+        oldFrames = new LinkedList<>();
+        handler = new Handler();
     }
 
     @Override
@@ -78,6 +88,7 @@ public class CameraScannerFragment extends Fragment implements CameraBridgeViewB
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         Mat gray = inputFrame.gray().clone();
+
         RotatedRect max = getBoundingRect(gray);
         Point[] pt = new Point[4];
         max.points(pt);
@@ -86,7 +97,38 @@ public class CameraScannerFragment extends Fragment implements CameraBridgeViewB
         Core.line(out, pt[1], pt[2], grayColor, 3);
         Core.line(out, pt[2], pt[3], grayColor, 3);
         Core.line(out, pt[3], pt[0], grayColor, 3);
+        double maxDiff = stabalize(gray);
+        Core.putText(out, "Max " + maxDiff, max.center, Core.FONT_HERSHEY_PLAIN, 2, black, 3);
+
+        if (maxDiff < 1) {
+            if (task == null || task.finished()) {
+                task = new Task(inputFrame.gray(), max);
+                task.start();
+            } else
+                Core.putText(out, "Running", new Point(max.center.x, max.center.y + 40), Core.FONT_HERSHEY_PLAIN, 2, black, 3);
+        }
         return out;
+    }
+
+    private double stabalize(Mat gray) {
+        double maxDiff = 0;
+        Mat toBeAdded = gray.clone();
+        Imgproc.GaussianBlur(toBeAdded, toBeAdded, new Size(5, 5), 10, 10);
+        double totalPixels = toBeAdded.size().area();
+        for (Mat oldFrame : oldFrames) {
+            maxDiff = Math.max(maxDiff, diff(toBeAdded, oldFrame) / totalPixels);
+        }
+        oldFrames.add(toBeAdded);
+        if (oldFrames.size() > 2)
+            oldFrames.remove();
+        return maxDiff;
+    }
+
+    public double diff(Mat newScreen, Mat oldScreen) {
+        Mat temp = new Mat(newScreen.size(), CvType.CV_8UC1);
+        Core.subtract(newScreen, oldScreen, temp);
+        Core.pow(temp, 2.0, temp);
+        return Core.sumElems(temp).val[0];
     }
 
     private RotatedRect getBoundingRect(Mat gray) {
@@ -109,5 +151,31 @@ public class CameraScannerFragment extends Fragment implements CameraBridgeViewB
             }
         }
         return max;
+    }
+
+    private class Task extends Thread {
+        private final Mat gray;
+        private final RotatedRect max;
+        private boolean finished;
+
+        public Task(Mat gray, RotatedRect max) {
+            this.gray = gray;
+            this.max = max;
+            finished = false;
+        }
+
+        public void run() {
+            super.run();
+            try {
+                sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            finished = true;
+        }
+
+        public boolean finished() {
+            return finished;
+        }
     }
 }
